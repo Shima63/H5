e9862f9efb3ad0cb2b81a993f36fa49de060eb7e
Shima <szzzdhrd@memphis.edu>
2015-03-02 15:55:30 -0600

    Program is working now

--- shima.cpp
+++ shima.cpp
@@ -9,6 +9,7 @@
  
 // External Libraries
 
+#include <stdlib.h>
 #include <cstdlib> 
 #include <iostream>
 #include <string>
@@ -23,6 +24,26 @@ using namespace std;
 
 string inputfilename, outputfilename = "shima.out", logfilename = "shima.log", message;
 
+// Defining Struct
+
+struct entry {
+    string network_code;
+    string station_code;
+    string type_of_band;
+    string type_of_instrument;
+    string orientation;
+};    
+    
+// Defining Enumerators
+    
+// start the enumeration from 1 to facilitate printing...
+// since months are numbered starting at 1
+
+enum months { 
+    January = 1, February, March, April, May, June,
+    July, August, September, October, November, December
+}; 
+ 
 // ********************************************************************************************************************
 
 // Function Prototype
@@ -30,12 +51,15 @@ string inputfilename, outputfilename = "shima.out", logfilename = "shima.log", m
 void open_input ( ifstream & );
 void open_file ( string, ofstream & );
 void print_file ( string, ofstream & );
+void print_file ( int, ofstream & );
 void check_date ( string, ofstream & );
+void check_month ( int, ofstream & );
 void check_time ( string, ofstream & );
 void check_time_zone ( string, ofstream & );
 void check_magnitude_type ( string, ofstream & );
 void check_magnitude_size ( float, ofstream & );
 string uppercase ( string );
+string monthstring ( months );
 
 // ********************************************************************************************************************
 
@@ -46,10 +70,15 @@ int main () {
 
     // Defining Variables' Type
     
-    string Event_ID, date, time, time_zone, earthquake_name, earthquake_name_continue, magnitude_type;
+    string Event_ID, date, time, time_zone, earthquake_name, earthquake_name_continue, magnitude_type, day, month, year;
+    string temp, temp1, temp2, temp3;
+    int num_of_valid_entries = 0, num_of_input = 0, flag = 0, value = 0, num_of_signal = 0;
     double longitude, latitude, depth;
     float magnitude_size;
-    //int num_stations = 300;
+    months month_name;
+   
+    entry entry_array [ 300 ];
+    entry entry_temp;
     
     // Prompt User for Input File Name.
 
@@ -61,11 +90,28 @@ int main () {
     ofstream logfile;
     open_file ( logfilename, logfile );
     
+    message = "Opening file: shima.in";
+    print_file ( message, logfile );
+    print_file ( "\n", logfile );
+    message = "Processing input ...";
+    print_file ( message, logfile );
+    print_file ( "\n", logfile );
+    
     // Reading and Checking Header
         
     inputfile >> Event_ID;
     inputfile >> date;   
     check_date ( date, logfile );
+    
+    // Finding and Checking  Validity of the Month
+    
+    temp =  temp.append ( date.begin() + 3 ,date.begin() + 5 );
+    value = atoi(temp.c_str());
+    check_month ( value, logfile );
+    month_name = months(value);
+    month = monthstring ( month_name );
+
+    
     inputfile >> time;
     check_time ( time, logfile );
     inputfile >> time_zone;
@@ -87,13 +133,216 @@ int main () {
     inputfile >> magnitude_size;
     check_magnitude_size ( magnitude_size, logfile );
 
-    
+    message = "Header read correctly!";
+    print_file ( message, logfile );
+    print_file ( "\n\n", logfile );
 
     // Preparing output file
     
     ofstream outputfile;
     open_file ( outputfilename, outputfile );
 
+    outputfile << "# " << day.append( date.begin(),date.begin() + 2 ) << " " << month << " " << year.append( date.begin() + 6,date.end() );
+    outputfile << " " << time << " " <<  time_zone << " " << magnitude_type << " " << magnitude_size << " " << earthquake_name << " ";
+    outputfile << "[" << Event_ID << "]  (" << longitude << ", " << latitude << ", " << depth << ")" << endl;
+
+    // Reading Entries
+
+    while ( ( inputfile >> entry_temp.network_code ) && ( num_of_valid_entries < 300 ) ) {
+        flag = 0;
+        num_of_input = num_of_input + 1;
+    
+        // Checking Network
+
+        if ( ( entry_temp.network_code.length() != 2 ) || ( ( entry_temp.network_code != "CE" ) && ( entry_temp.network_code != "CI" ) && ( entry_temp.network_code != "FA" ) && ( entry_temp.network_code != "NP" ) && ( entry_temp.network_code != "WR" ) ) ) {
+            flag = 1;
+        }
+        if ( flag == 1) {
+            print_file ( "Entry # ", logfile );
+            print_file ( num_of_input, logfile );
+            print_file ( " ignored. Invalid network. ", logfile );
+            print_file ( "\n", logfile );
+        }
+        
+        // Checking Station Code
+        
+        inputfile >> entry_temp.station_code;        
+        if ( entry_temp.station_code.length() != 3 ) {
+            if ( entry_temp.station_code.length() != 5 ) {
+                flag = 2;
+            }
+            else {
+                if ( ( !isdigit ( entry_temp.station_code[0] ) ) || ( !isdigit ( entry_temp.station_code[1] ) ) || ( !isdigit ( entry_temp.station_code[2] ) ) || ( !isdigit ( entry_temp.station_code[3] ) ) || ( !isdigit ( entry_temp.station_code[4] ) ) ) {
+                    flag = 2;
+                }    
+            }
+        }
+        else {
+            if ( ( !isalpha ( entry_temp.station_code[0] ) ) || ( !isalpha ( entry_temp.station_code[1] ) ) || ( !isalpha ( entry_temp.station_code[2] ) ) ) {
+                flag = 2;
+            }
+            else {
+                if ( uppercase ( entry_temp.station_code ) != entry_temp.station_code ) {
+                    flag = 2;
+                }    
+            }
+        }        
+        if ( flag == 2) {
+            print_file ( "Entry # ", logfile );
+            print_file ( num_of_input, logfile );
+            print_file ( " ignored. Invalid station code. ", logfile ); 
+            print_file ( "\n", logfile );
+        }
+        
+        
+        // Checking Type of Band
+        
+        inputfile >> entry_temp.type_of_band;        
+        if ( ( uppercase ( entry_temp.type_of_band ) != uppercase ( "Long-period" ) ) && ( uppercase ( entry_temp.type_of_band ) != uppercase ( "Short-period" ) ) && ( uppercase ( entry_temp.type_of_band ) != uppercase ( "Broadband" ) ) ) {
+            flag = 3;
+        }
+        if ( flag == 3 ) {
+            print_file ( "Entry # ", logfile );
+            print_file ( num_of_input, logfile );
+            print_file ( " ignored. Invalid type of band. ", logfile ); 
+            print_file ( "\n", logfile );
+        }
+        
+        // Checking Type of Instrument
+        
+        inputfile >> entry_temp.type_of_instrument;        
+        if ( ( uppercase ( entry_temp.type_of_instrument ) != uppercase ( "High-Gain" ) ) && ( uppercase ( entry_temp.type_of_instrument ) != uppercase ( "Low-Gain" ) ) && ( uppercase ( entry_temp.type_of_instrument ) != uppercase ( "Accelerometer" ) ) ) {
+            flag = 4;
+        }
+        if ( flag == 4 ) {
+            print_file ( "Entry # ", logfile );
+            print_file ( num_of_input, logfile );
+            print_file ( " ignored. Invalid type of instrument. ", logfile ); 
+            print_file ( "\n", logfile );
+        }
+        
+        // Checking Orientation
+        
+        temp1 = "";
+        temp2 = "";
+        temp3 = "";
+        inputfile >> entry_temp.orientation;        
+        if ( ( entry_temp.orientation.length() < 1 ) ||  ( entry_temp.orientation.length() > 3 ) ) {
+            flag = 5;
+        }
+        else {
+            temp1 = entry_temp.orientation[0];
+            if ( ( temp1 != "1" ) && ( temp1 != "2" ) && ( temp1 != "3" ) ) {
+                if ( ( uppercase ( temp1 ) != uppercase ( "N" ) ) && ( uppercase ( temp1 ) != uppercase ( "E" ) ) && ( uppercase ( temp1 ) != uppercase ( "Z" ) ) ) { 
+                    flag = 5;
+                }
+                else {
+                    if ( entry_temp.orientation.length() > 1 ) {
+                        temp2 = entry_temp.orientation[1];
+                        if ( ( temp2 != "N" ) && ( temp2 != "E" ) && ( temp2 != "Z" ) ) {
+                            flag = 5;
+                        }
+                        else {
+                            if ( entry_temp.orientation.length() > 2 ) {
+                                temp3 = entry_temp.orientation[2];
+                                if ( ( temp3 != "N" ) && ( temp3 != "E" ) && ( temp3 != "Z" ) ) {
+                                    flag = 5;
+                                } 
+                            }
+                        }
+                    }
+                }
+            }
+            else {
+                if ( entry_temp.orientation.length() > 1 ) {
+                    temp2 = entry_temp.orientation[1];
+                    if ( ( temp2 != "1" ) && ( temp2 != "2" ) && ( temp2 != "3" ) ) {
+                        flag = 5;
+                    }
+                    else {
+                        if ( entry_temp.orientation.length() > 2 ) {
+                            temp3 = entry_temp.orientation[2];
+                            if ( ( temp3 != "1" ) && ( temp3 != "2" ) && ( temp3 != "3" ) ) {
+                                flag = 5;
+                            } 
+                        }
+                    }
+                }
+            }
+        }            
+            
+       if ( flag == 5 ) {
+            print_file ( "Entry # ", logfile );
+            print_file ( num_of_input, logfile );
+            print_file ( " ignored. Invalid orientation. ", logfile ); 
+            print_file ( "\n", logfile );  
+        }
+        if ( flag == 0 ) {   
+            num_of_valid_entries = num_of_valid_entries + 1;
+            entry_temp.orientation = temp1;
+            entry_array [ num_of_signal ] = entry_temp;
+            num_of_signal = num_of_signal + 1;
+            if ( temp2 != "" ) {
+                entry_temp.orientation = temp2;
+                entry_array [ num_of_signal ] = entry_temp;
+                num_of_signal = num_of_signal +1;
+                if ( temp3 != "" ) {
+                    entry_temp.orientation = temp3;
+                    entry_array [ num_of_signal ] = entry_temp;
+                    num_of_signal = num_of_signal +1;
+                }
+            }
+        }            
+    }
+
+    print_file ( "Total invalid entries ignored: ", logfile );
+    print_file ( ( num_of_input - num_of_valid_entries ), logfile );
+    print_file ( "\n", logfile );
+    print_file ( "Total valid entries read: ", logfile );
+    print_file ( num_of_valid_entries, logfile );
+    print_file ( "\n", logfile );
+    print_file ( "Total signal names produced: ", logfile );
+    print_file ( num_of_signal, logfile );
+    print_file ( "\n", logfile );
+    print_file ( "Finished!", logfile );
+ 
+    outputfile << ( num_of_signal ) << endl;
+    for (int i = 0; i < ( num_of_signal ); i++) {
+    
+     // Changing Names
+    
+    if ( uppercase ( entry_array[i].type_of_band ) == uppercase ( "Long-period" ) ) {
+        entry_array[i].type_of_band = "L";
+    }    
+    if ( uppercase ( entry_array[i].type_of_band ) == uppercase ( "Short-period" ) ) {
+        entry_array[i].type_of_band = "B";
+    }
+    if ( uppercase ( entry_array[i].type_of_band ) == uppercase ( "broadband" ) ) {
+        entry_array[i].type_of_band = "H";
+    }   
+    if ( uppercase ( entry_array[i].type_of_instrument ) == uppercase ( "High-Gain" ) ) {
+        entry_array[i].type_of_instrument = "H";
+    }    
+    if ( uppercase ( entry_array[i].type_of_instrument ) == uppercase ( "Low-Gain" ) ) {
+        entry_array[i].type_of_instrument = "L";
+    }
+    if ( uppercase ( entry_array[i].type_of_instrument ) == uppercase ( "Accelerometer" ) ) {
+        entry_array[i].type_of_instrument = "N";
+    } 
+    
+        temp= "";
+        temp.append(Event_ID);
+        temp.append(".");
+        temp.append(entry_array[i].network_code);
+        temp.append(".");
+        temp.append(entry_array[i].station_code);
+        temp.append(".");
+        temp.append(entry_array[i].type_of_band);
+        temp.append(entry_array[i].type_of_instrument);
+        temp.append(entry_array[i].orientation);
+        outputfile << temp << endl;    
+    }
+    
     return 0;
 }
 
@@ -152,6 +401,14 @@ void print_file ( string message, ofstream & ofs ) {
     return;
 }
 
+// This "print_file" function Prints numbers on Files and Terminal. Number Is of Int Type.
+
+void print_file ( int number, ofstream & ofs ) {
+    ofs << number << flush;
+    cout << number << flush;
+    return;
+}
+
 // This "check_date" function checks the validity of date entry.
 
 void check_date ( string date, ofstream & logfile ) {
@@ -175,6 +432,17 @@ void check_date ( string date, ofstream & logfile ) {
                 exit (EXIT_FAILURE);
             }
         }
+    }
+    return;
+}    
+
+// This "check_month" function checks the validity of month entry.
+
+void check_month ( int month, ofstream & logfile ) {
+    if ( ( month > 12 ) || ( month < 1 ) ) {
+        message = "Month format is not right.";
+        print_file ( message, logfile );
+        exit (EXIT_FAILURE);
     }
     return;
 }    
@@ -210,10 +478,11 @@ void check_time ( string time, ofstream & logfile ) {
 
 void check_time_zone ( string time_zone, ofstream & logfile ) {
     message = "Time_zone format is not right.";
-    if ( time_zone.length() != 3 ) {
+    if ( ( time_zone.length() != 3 ) || ( !isalpha ( time_zone[0] ) ) || ( !isalpha ( time_zone[1] ) ) || ( !isalpha ( time_zone[2] ) ) ) {
         print_file ( message, logfile );
         exit (EXIT_FAILURE);
     }
+    
     return;
 }    
 
@@ -240,7 +509,7 @@ void check_magnitude_size ( float magnitude_size, ofstream & logfile ) {
     return;
 }
 
-// This "string uppercase" function changes all the lettera of the input string to upper case.
+// This "string uppercase" function changes all the letters of the input string to upper case. ( For string )
 
 string uppercase ( string s ) {
     string result = s;
@@ -249,4 +518,35 @@ string uppercase ( string s ) {
     return result;
 }
 
-    
+// Function to Convert from "month" to a String...
+
+string monthstring (months month) {
+    switch (month) {
+        case January:
+            return "January";
+        case February:
+            return "February";
+        case March:
+            return "March";
+        case April:
+            return "April";
+        case May:
+            return "May";
+        case June:
+            return "June";
+        case July:
+            return "July";
+        case August:
+            return "August";
+        case September:
+            return "September";
+        case October:
+            return "October";
+        case November:
+            return "November";
+        case December:
+            return "December";
+        default:
+            return "ILLEGAL";
+    }
+}
